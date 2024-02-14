32b97a1a5b35a2f9b7df5df04b319dcf9dfe1d9a
ahmed-ray <157624749+ahmed-ray@users.noreply.github.com>
2024-02-14 20:54:29 +0500

    Update rayyan.py

--- rayyan.py
+++ rayyan.py
@@ -1,4 +1,5 @@
 import streamlit as st
 st.title("Welcome Bano Qabil")
-st.write("Pythan CIT  by Sir Ghufran Kamaluddin")
+st.write("Pythan CIT  by rayyan ahmed")
 st.title("Campus: PCHMC")
+st.write("TIMING 8 TO 10")
